e15d1d7990c0e305c874431377d2596abd3686b2
Wasim <mohammed.wasimuddin@rewe-digital.com>
2023-01-16 11:45:44 +0100

    Add Project structure

---
+++ src/test/kotlin/com/paxier/currencyexchange/CurrencyexchangeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.paxier.currencyexchange
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CurrencyexchangeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
